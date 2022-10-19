55d37cab5561566ab108ebde409a94661d33c40f
Innex42 <innex4@gmail.com>
2022-10-19 09:59:24 +0100

    Added Accordian Bootstrap

--- lab2/src/App.js
+++ lab2/src/App.js
@@ -1,7 +1,14 @@
-import React from 'react';
+import React from "react";
+import { items } from "./data/data";
+import DisplayFoodItems from "./DisplayFoodItems";
+
+console.log( {items})
 function App() {
-return (
- <p>hello world</p>
- );
+  return (
+    <div>
+      <h1>Menu</h1>
+      <DisplayFoodItems foodList={items} />
+    </div>
+  );
 }
 export default App;
---
+++ lab2/src/DisplayFoodItems.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Accordion from "react-bootstrap/Accordion"
+import FoodItem from './FoodItem';
+
+const DisplayFoodItems = ({foodList }) => {
+
+    return (
+        <Accordion style={{width:50+'%'}}>
+            {foodList.map((food, index) => {
+                return (
+                    <Accordion.Item eventKey={index} key={index}>
+                        <FoodItem food={food}/>
+                    </Accordion.Item>
+                )
+            })}
+        </Accordion>
+    );
+};
+export default DisplayFoodItems;
