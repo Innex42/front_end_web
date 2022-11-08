96240efdca55a294b1df6f5f474041b5d7d65e9f
Innex42 <innex4@gmail.com>
2022-11-08 11:27:04 +0000

    inital commit

---
+++ sasslab2/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+import Container from "react-bootstrap/Container";
+
+function App() {
+  return (
+    <div className='container'>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      
+    </div>
+  );
+}
+
+export default App;
