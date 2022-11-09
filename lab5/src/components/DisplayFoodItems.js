271c0ace5f6f8d3ab871f9af1c22c4bcd714fe94
Innex42 <innex4@gmail.com>
2022-11-09 09:56:49 +0000

    shown nutrition data for food items

--- lab5/src/components/DisplayFoodItems.js
+++ lab5/src/components/DisplayFoodItems.js
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Item from "./Item";
+import FetchData from './FetchNutrition'; 
 import {items} from '../data/Data'
 
 const DisplayFoodItems = () => {
-
-    const handleClick= (e) => {
-        alert(e.target.innerHTML)
+    const [selectedItem,setSelectedItem] = useState("")
+    const handleClick= (currentItem) => {
+        setSelectedItem(currentItem)
     } 
     
     return (
+        <>
         <ul>
+        <h2>Menu</h2>
             {items.map((food) => {
                 return(
-                    <li key={food.id} onClick={handleClick} > <Item item={food} /></li>
+                    <li key={food.id} onClick={() => handleClick(food)} > 
+                    <Item item={food} /></li>
                 )
             })}
         </ul>
+        <h2>Food: {selectedItem.name}</h2>
+        <FetchData query={selectedItem.name}/>
+        </>
     );
 };
 export default DisplayFoodItems;
